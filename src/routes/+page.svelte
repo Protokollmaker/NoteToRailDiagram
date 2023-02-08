280ced18af44c0165c8da61b97f9b74fa9f40ad1
Protokollmaker <rwSpammail1@gmail.com>
2023-02-08 16:46:49 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let city: string = 0;
+    city = 0; // OK
+    type Color = 'red' | 'green' | 'blue';
+    let c: Color = 'red';
+    c = 2;
+</script>
+{c}
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
