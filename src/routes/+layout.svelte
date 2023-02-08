04e4332686646df4e4fd18f267baa196d3451080
Protokollmaker <rwSpammail1@gmail.com>
2023-02-08 19:54:51 +0100

    befor seting up Vittest

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+	import './styles.css';
+	
+	import {onMount} from "svelte";
+	onMount(() => {
+		
+	});
+</script>
+
+<div class="app">
+	<Header />
+
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<header>
+	<nav>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/debug' ? 'page' : undefined}>
+				<a href="/debug">Debug</a>
+            </li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+ul li {
+    display:inline;
+}
+
+a {
+	margin: 2px 2px 2px 10px;
+}
+</style>
